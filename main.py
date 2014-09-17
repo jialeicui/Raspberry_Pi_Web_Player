25a295e969b3140850da01aef59581a9668b77e8
jialei3 <jialei3@staff.sina.com.cn>
2014-09-17 11:05:44 +0800

    play by mplayer

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import web
 import song_scan
 import os
+import subprocess
 
 render = web.template.render('templates/')
 base_dir = '/Users/jialeicui/Music'
+mplayer_sub = None
 
 urls = (
     '/', 'index',
@@ -18,8 +20,12 @@ class index:
 
 class play:
     def GET(self, opt_dir = ''):
+        mp = globals()['mplayer_sub']
         # return os.path.join(base_dir, opt_dir)
-        return os.system('open "' + os.path.join(base_dir, opt_dir) + '"')
+        if mp:
+            mp.terminate()
+        globals()['mplayer_sub'] = subprocess.Popen('mplayer "' + os.path.join(base_dir, opt_dir) + '"', shell=True)
+        return ''
 
 if __name__ == "__main__": 
     app = web.application(urls, globals())
