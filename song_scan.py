4e4db63e94b45a58125087e7ae5dcc6561bf8e75
jialei3 <jialei3@staff.sina.com.cn>
2014-09-17 12:30:49 +0800

    add css def

--- song_scan.py
+++ song_scan.py
@@ -6,10 +6,11 @@ def get(base_dir, opt_dir = ''):
     if os.path.isfile(final_dir):
         return  play(final_dir)
     for dirs in os.listdir(final_dir):
-        if os.path.isdir(os.path.join(final_dir, dirs)):
-            ret.append({'name':dirs, 'href':'ls/' + os.path.join(opt_dir, dirs)})
-        else:
-            ret.append({'name':dirs, 'href':'/play/' + os.path.join(opt_dir, dirs)})
+        if not dirs.startswith('.'):
+            if os.path.isdir(os.path.join(final_dir, dirs)):
+                ret.append({'name':dirs, 'href':'ls/' + os.path.join(opt_dir, dirs), 'playable':False})
+            else:
+                ret.append({'name':dirs, 'href':'/play/' + os.path.join(opt_dir, dirs), 'playable':True})
     return ret
     pass
 
