73e276d5999fdfbd65a650d989a157e4d216e179
jialei3 <jialei3@staff.sina.com.cn>
2014-09-17 23:47:45 +0800

    check playable

--- song_scan.py
+++ song_scan.py
@@ -6,7 +6,7 @@ def get(base_dir, opt_dir = ''):
     if os.path.isfile(final_dir):
         return  play(final_dir)
     for dirs in os.listdir(final_dir):
-        if not dirs.startswith('.'):
+        if not dirs.startswith('.') and _is_audio_file(dirs):
             if os.path.isdir(os.path.join(final_dir, dirs)):
                 ret.append({'name':dirs, 'href':'/ls/' + os.path.join(opt_dir, dirs), 'playable':False})
             else:
@@ -16,4 +16,12 @@ def get(base_dir, opt_dir = ''):
 
 def play(f):
     return []
+    pass
+
+def _is_audio_file(name):
+    valid_ext = ['mp3', 'm4a', 'ape', 'wma']
+    n, ext = os.path.splitext(name)
+    if valid_ext.count(ext[1:].lower()) != 0:
+        return True
+    return False
     pass
