29d5765be3b09b79257867f6ad9e2def574cf99e
jialei3 <jialei3@staff.sina.com.cn>
2014-09-17 20:32:09 +0800

    fix bug

--- song_scan.py
+++ song_scan.py
@@ -8,7 +8,7 @@ def get(base_dir, opt_dir = ''):
     for dirs in os.listdir(final_dir):
         if not dirs.startswith('.'):
             if os.path.isdir(os.path.join(final_dir, dirs)):
-                ret.append({'name':dirs, 'href':'ls/' + os.path.join(opt_dir, dirs), 'playable':False})
+                ret.append({'name':dirs, 'href':'/ls/' + os.path.join(opt_dir, dirs), 'playable':False})
             else:
                 ret.append({'name':dirs, 'href':'/play/' + os.path.join(opt_dir, dirs), 'playable':True})
     return ret
